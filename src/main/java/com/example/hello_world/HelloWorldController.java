6634e45f7a93b2fee35494b95c45a37546ff5ded
wimdavies <william.johnston.davies@gmail.com>
2024-05-30 13:53:18 +0100

    Project setup

---
+++ src/main/java/com/example/hello_world/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.hello_world;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("home");
+    }
+}
